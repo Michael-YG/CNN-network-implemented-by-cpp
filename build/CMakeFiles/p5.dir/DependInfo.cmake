8cb4874659448a1353e351f29662700b389fabbf
Michael-YG <61082217+Michael-YG@users.noreply.github.com>
2022-03-14 17:22:00 +0800

    update build

---
+++ build/CMakeFiles/p5.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/michael/OneDrive/codes/c&cpp/project/p5/MyMatrix.cpp" "CMakeFiles/p5.dir/MyMatrix.cpp.o" "gcc" "CMakeFiles/p5.dir/MyMatrix.cpp.o.d"
+  "/Users/michael/OneDrive/codes/c&cpp/project/p5/cnn.cpp" "CMakeFiles/p5.dir/cnn.cpp.o" "gcc" "CMakeFiles/p5.dir/cnn.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/p5.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/p5.dir/MyMatrix.cpp.o"
+  "CMakeFiles/p5.dir/MyMatrix.cpp.o.d"
+  "CMakeFiles/p5.dir/cnn.cpp.o"
+  "CMakeFiles/p5.dir/cnn.cpp.o.d"
+  "p5"
+  "p5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/p5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
